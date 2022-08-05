500abf09bb7271ddf1c1b42d9f458a94fafbdc11
Tobi Lehman <tobi.lehman@suse.com>
2022-08-05 10:01:38 -0700

    Add CRD as Go Type

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/harvester/pcidevices
+
+go 1.18
---
+++ pkg/apis/pcidevices.harvesterhci.io/v1beta1/pcidevice.go
@@ -0,0 +1,33 @@
+package v1beta1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +genclient
+// +genclient:nonNamespaced
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:object:root=true
+
+// PCIDevice is the Schema for the pcidevices API
+type PCIDevice struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PCIDeviceSpec   `json:"spec,omitempty"`
+	Status PCIDeviceStatus `json:"status,omitempty"`
+}
+
+type PCIDeviceSpec struct {
+}
+
+// PCIDeviceStatus defines the observed state of PCIDevice
+type PCIDeviceStatus struct {
+	Address           string   `json:"address"`
+	VendorId          string   `json:"vendorId"`
+	DeviceId          string   `json:"deviceId"`
+	Node              Node     `json:"node"`
+	Description       string   `json:"description"`
+	KernelDriverInUse string   `json:"kernelDriverInUse,omitempty"`
+	KernelModules     []string `json:"kernelModules"`
+}
