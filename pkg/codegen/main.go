d04d1ae6b54e5dd9432c00d4d3c379f301ccd307
Tobi Lehman <tobi.lehman@suse.com>
2022-08-05 11:42:32 -0700

    add codegen main

---
+++ pkg/codegen/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	controllergen "github.com/rancher/wrangler/pkg/controller-gen"
+	"github.com/rancher/wrangler/pkg/controller-gen/args"
+
+	// Ensure gvk gets loaded in wrangler/pkg/gvk cache
+	_ "github.com/rancher/wrangler/pkg/generated/controllers/apiextensions.k8s.io/v1"
+)
+
+func main() {
+	os.Unsetenv("GOPATH")
+	controllergen.Run(
+		args.Options{
+			OutputPackage: "github.com/harvester/pcidevices",
+			Boilerplate:   "scripts/boilerplate.go.txt",
+			Groups: map[string]args.Group{
+				"devices.harvesterhci.io": {
+					Types: []any{
+						"./pkg/apis/devices.harvesterhci.io/v1beta1",
+					},
+					GenerateTypes: true,
+				},
+			},
+		})
+}
