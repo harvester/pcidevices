d3a07917d59afbf72ddd07d20e39d47f105279f3
Gaurav Mehta <gaurav@rancher.com>
2023-02-22 14:57:22 +1100

    additional logic to copy over username from existing pvcclaim to ensure user can re-use auto created pvcclaims in the future

--- pkg/webhook/indexer.go
+++ pkg/webhook/indexer.go
@@ -2,7 +2,6 @@ package webhook
 
 import (
 	"fmt"
-	"strings"
 
 	kubevirtv1 "kubevirt.io/api/core/v1"
 
@@ -21,19 +20,6 @@ func RegisterIndexers(clients *Clients) {
 	deviceCache := clients.PCIFactory.Devices().V1beta1().PCIDevice().Cache()
 	deviceCache.AddIndexer(PCIDeviceByResourceName, pciDeviceByResourceName)
 	deviceCache.AddIndexer(IommuGroupByNode, iommuGroupByNodeName)
-}
-
-func pciClaimByVMName(obj *v1beta1.PCIDeviceClaim) ([]string, error) {
-	if len(obj.OwnerReferences) != 0 {
-		for _, v := range obj.OwnerReferences {
-			if strings.ToLower(v.Kind) == "virtualmachine" {
-				return []string{v.Name}, nil
-			}
-
-		}
-	}
-
-	return []string{}, nil
 }
 
 func vmByName(obj *kubevirtv1.VirtualMachine) ([]string, error) {
